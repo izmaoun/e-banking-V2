5e698cce508fb988096d77e3b917702804494aa2
Amine <beleamine84@gmail.com>
2026-07-11 18:40:31 +0100

    Fix API URL for Tailscale

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
 // frontend/src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'http://e_bank_backend:8085',
-  backendHost: 'http://e_bank_backend:8085'  // Ajouter cette ligne
+  //apiUrl: 'http://e_bank_backend:8085',
+  //backendHost: 'http://e_bank_backend:8085'  // Ajouter cette ligne
+
+  apiUrl: 'http://100.98.75.119:8085',  // ← Même chose
+  backendHost: 'http://100.98.75.119:8085'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,10 @@
 // frontend/src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8085',
-  backendHost: 'http://localhost:8085'  // Ajouter cette ligne
+  //apiUrl: 'http://localhost:8085',
+  //backendHost: 'http://localhost:8085'  // Ajouter cette ligne
+
+  apiUrl: 'http://100.98.75.119:8085',  // ← Utiliser l'IP Tailscale
+  backendHost: 'http://100.98.75.119:8085'
+
 };
