70d769f725e75013c1df52cb7f60e6854b00c012
Amine <beleamine84@gmail.com>
2026-07-15 10:34:51 +0100

    test7

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   //apiUrl: 'http://e_bank_backend:8085',
   //backendHost: 'http://e_bank_backend:8085'  // Ajouter cette ligne
 
-  apiUrl: 'http://100.94.194.36:8085', // ← Même chose
-  backendHost: 'http://100.94.194.36:8085'
+  apiUrl: 'http://100.64.178.55:8085', // ← Même chose
+  backendHost: 'http://100.64.178.55:8085'
 };
